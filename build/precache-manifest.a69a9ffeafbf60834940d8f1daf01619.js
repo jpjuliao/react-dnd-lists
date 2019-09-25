00382b4902eef0baafbf2840f2bfb377bd0dfc52
jpjuliao <jpjuliao@gmail.com>
2019-09-25 04:18:45 -0500

    include build folder

---
+++ build/precache-manifest.a69a9ffeafbf60834940d8f1daf01619.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe88937fcef2b20fa13c456eaf44231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f88888e5b78e1085996",
+    "url": "/static/css/main.28866cdd.chunk.css"
+  },
+  {
+    "revision": "8318d5dcca72e4001bf9",
+    "url": "/static/js/2.517ef760.chunk.js"
+  },
+  {
+    "revision": "7f88888e5b78e1085996",
+    "url": "/static/js/main.abdb3b18.chunk.js"
+  },
+  {
+    "revision": "4fe2417b5d006f9abb79",
+    "url": "/static/js/runtime-main.cae08996.js"
+  }
+]);
